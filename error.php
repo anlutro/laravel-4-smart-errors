43034562319e34a4959d454e6e1b06c9d0223e2d
Andreas Lutro <andreas@hjelseth.com>
2013-08-05 15:52:53 +0200

    added license

--- error.php
+++ error.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Laravel 4 Smart Errors
+ *
+ * @author    Andreas Lutro <anlutro@gmail.com>
+ * @license   http://opensource.org/licenses/MIT
+ * @package   Laravel 4 Smart Errors
+ */
+
 // generic error handler
 App::error(function(Exception $exception, $code)
 {
