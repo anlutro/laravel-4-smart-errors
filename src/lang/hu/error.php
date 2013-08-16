b1168da9e13e0af59a58d35c06ffa3cafa6fc0b4
Barna Szalai <b.sz@devartpro.com>
2013-08-16 15:31:38 +0200

    Added hungarian lang file

---
+++ src/lang/hu/error.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Laravel 4 Smart Errors
+ *
+ * @author    Andreas Lutro <anlutro@gmail.com>
+ * @license   http://opensource.org/licenses/MIT
+ * @package   Laravel 4 Smart Errors
+ */
+
+return array(
+	'genericErrorTitle' => 'Hiba történt!',
+	'genericErrorParagraph1' => 'Hiba történt a látogatott weboldalon, ezért a futtatása le lett állítva.',
+	'genericErrorParagraph2' => 'A fejlesztő automatikusan értesítve lett, és megpróbálja megelőzni a jövőben az ilyen hibaeseményeket.Elnézést az okozott kellemetlenségekért.',
+	'missingTitle' => 'Az oldal nem található',
+	'missingText' => 'A kért oldal nem található.',	
+	'backLinkTitle' => 'Vissza',
+	'frontpageLinkTitle' => 'Főoldal',
+);
