90a85f0a2109a3758a86f920e12fe72f115b2546
Johntaa <fccffccffccf@yahoo.com>
2013-08-26 19:47:27 +0300

    Create error.php

---
+++ src/lang/ar/error.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* Laravel 4 Smart Errors
+*
+* @author Andreas Lutro <anlutro@gmail.com>
+* @license http://opensource.org/licenses/MIT
+* @package Laravel 4 Smart Errors
+*/
+
+return array(
+'genericErrorTitle' => '! حدث خطأ ',
+'genericErrorParagraph1' => 'هناك خطأ يمنع الموقع من الاستمرارية',
+'genericErrorParagraph2' => 'نعتذر للخطأ ،، وقد م ابلاغ المسئول عن الموقع لتلافي ذلك في المستقبل',
+
+'missingTitle' => 'الصفحة المطلوبة غير موجودة',
+'missingText' => 'الصفحة المطلوبة غير متوفرة ',
+
+'backLinkTitle' => 'الرجوع',
+'frontpageLinkTitle' => 'الرئيسية',
+);
