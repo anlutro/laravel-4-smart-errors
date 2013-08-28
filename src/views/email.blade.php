abfd8576041c028168b908444a4de0e77c3d765f
Andreas Lutro <anlutro@gmail.com>
2013-08-28 15:50:44 +0200

    new, cleaner view

--- src/views/email.blade.php
+++ src/views/email.blade.php
@@ -4,8 +4,35 @@
 	Route: {{ $route }}
 </p>
 
-<?php var_dump($exception) ?>
+@if ()
+
+<p>
+	Error message: {{ $e->getMessage() }}
+	@if ($e->getCode() > 0)
+	 - code: {{ $e->getCode() }}
+	@endif
+	<br>In {{ $e->getFile() }} on line {{ $e->getLine() }}
+</p>
+
+<p>
+	<b>Stack trace</b><br>
+	{{ nl2br($e->getTraceAsString()) }}
+</p>
+
+@if ($pe = $e->getPrevious())
+<p>
+	<b>Previous exception:</b> {{ $pe->getMessage() }}
+	@if ($pe->getCode() > 0)
+	 - code: {{ $pe->getCode() }}
+	@endif
+	<br>In {{ $pe->getFile() }} on line {{ $pe->getLine() }}
+</p>
+@endif
+
 
 @if ($input)
-<?php var_dump($input) ?>
+<p>
+	<b>Input</b><br>
+	<?php var_dump($input) ?>
+</p>
 @endif
