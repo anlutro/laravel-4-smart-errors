9659f5a62b768f30af8c6f39495394b5918e06c7
alxy <alexander@familie-guth.com>
2013-11-26 17:23:36 +0100

    German translation

---
+++ src/lang/de/error.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Laravel 4 Smart Errors
+ *
+ * @author    Andreas Lutro <anlutro@gmail.com>
+ * @license   http://opensource.org/licenses/MIT
+ * @package   Laravel 4 Smart Errors
+ */
+
+return array(
+        'genericErrorTitle' => 'Ein Fehler ist aufgetreten!',
+        'genericErrorParagraph1' => 'Es ist ein Fehler auf der aktuellen Seite aufgetreten.',
+        'genericErrorParagraph2' => 'Der Administrator wurde automatisch benachrichtigt und wird versuchen, diesen Fehler in Zukunft zu beheben. Wir entschuldigen uns für entstandenen Unannehmlichkeiten!',
+
+        'missingTitle' => 'Seite nicht gefunden',
+        'missingText' => 'Die angeforderte Seite konnte nicht gefunden werden.',
+        
+        'backLinkTitle' => 'Zurück',
+        'frontpageLinkTitle' => 'Startseite',
+);
