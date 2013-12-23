304e91e9e450966fee9fc348bbcf08e52b3fa83c
Bernhard Breytenbach <bernhard@coffeecode.co.za>
2013-12-23 08:32:58 +0200

    Fix spelling mistake occured => occurred

--- src/lang/en/error.php
+++ src/lang/en/error.php
@@ -8,13 +8,13 @@
  */
 
 return array(
-	'genericErrorTitle' => 'An error occured!',
-	'genericErrorParagraph1' => 'An error occured on the site you were visiting, stopping it from doing its job.',
+	'genericErrorTitle' => 'An error occurred!',
+	'genericErrorParagraph1' => 'An error occurred on the site you were visiting, stopping it from doing its job.',
 	'genericErrorParagraph2' => 'The developer has been notified automatically and will try to prevent the error from happening in the future. We apologize for any inconvenience caused.',
 
 	'missingTitle' => 'Page not found',
 	'missingText' => 'The page you requested could not be found.',
-	
+
 	'backLinkTitle' => 'Go back',
 	'frontpageLinkTitle' => 'Front page',
 );
