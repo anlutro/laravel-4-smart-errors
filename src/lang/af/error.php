fd8786ba775b2be42e71477239b8a3d0edbc0294
Bernhard Breytenbach <bernhard@coffeecode.co.za>
2013-12-23 08:33:49 +0200

    Add language file for Afrikaans

---
+++ src/lang/af/error.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Laravel 4 Smart Errors
+ *
+ * @author    Andreas Lutro <anlutro@gmail.com>
+ * @license   http://opensource.org/licenses/MIT
+ * @package   Laravel 4 Smart Errors
+ */
+
+return array(
+	'genericErrorTitle' => '\'n Fout het plaasgevind!',
+	'genericErrorParagraph1' => '\'n Fout het plaasgevind op die webwerf tydens u besoek, en het verhoed om die huidige taak te voltooi.',
+	'genericErrorParagraph2' => 'Die ontwikkelaar is in kennis gestel en sal probeer verhoed dat die voud in die toekoms voorkom. Ons vra om verskoning vir enige ongerief wat hierdeur veroorsaak mag wees.',
+
+	'missingTitle' => 'Bladsy nie gevind nie',
+	'missingText' => 'Die bladsy wat u versoek het kon nie gevind word nie.',
+
+	'backLinkTitle' => 'Keer terug',
+	'frontpageLinkTitle' => 'Voorblad',
+);
