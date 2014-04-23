eb7f83b84ab3af30eedf0f01f58d016dfce04961
= <=>
2014-04-23 21:55:00 +0200

    added dutch language file

--- src/lang/nl/error.php
+++ src/lang/nl/error.php
@@ -1,7 +1,20 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Dirk
- * Date: 23-4-2014
- * Time: 21:52
- */ 
+ * Laravel 4 Smart Errors
+ *
+ * @author Andreas Lutro <anlutro@gmail.com>
+ * @license http://opensource.org/licenses/MIT
+ * @package Laravel 4 Smart Errors
+ */
+
+return array(
+    'genericErrorTitle' => 'Er is een fout opgetreden!',
+    'genericErrorParagraph1' => 'Er is een fout opgetreden op de site die u bezocht, waardoor uw verzoek niet verder kon worden afgehandeld.',
+    'genericErrorParagraph2' => 'De beheerder van deze site is op de hoogte gesteld en zal er voor zorgen dat deze fout in de toekomst wordt verholpen. Wij bieden onze excuses aan voor het ongemak.',
+
+    'missingTitle' => 'Pagina niet gevonden.',
+    'missingText' => 'De door u opgevraagde pagina kon niet worden gevonden.',
+
+    'backLinkTitle' => 'Vorige pagina',
+    'frontpageLinkTitle' => 'Begin pagina',
+);
