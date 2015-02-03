3ae4c0d2eec2fb8c3cc8780068c2584f7681f8b1
Rafa Gómez <dotzecker@gmail.com>
2015-02-03 14:33:29 +0100

    Add Spanish translation

---
+++ src/lang/es/error.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Laravel 4 Smart Errors
+ *
+ * @author    Andreas Lutro <anlutro@gmail.com>
+ * @license   http://opensource.org/licenses/MIT
+ * @package   Laravel 4 Smart Errors
+ */
+
+return array(
+	'genericErrorTitle' => '¡Ha ocurrido un error!',
+	'genericErrorParagraph1' => 'Se ha producidoun error en la página que estabas visitando impidiendo su normal uso.',
+	'genericErrorParagraph2' => 'El equipo técnico ha sido notificado automáticamente y tratará de evitar que el error vuelva a ocurrir. Nos disculpamos por cualquier inconveniente causado.',
+
+	'missingTitle' => 'Página no encontrada',
+	'missingText' => 'La página solicitada no pudo ser encontrada.',
+
+	'csrfTitle' => 'Solicitud inválida',
+	'csrfText' => 'No hemos podido verificar la autenticidad de su solicitud ya que la sesión ha caducado. Por favor, vuelva a intentarlo.',
+
+	'backLinkTitle' => 'Vuelve',
+	'frontpageLinkTitle' => 'Página principal',
+);
